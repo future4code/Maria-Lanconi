43970b76e5ec9d6526c48bc36ce3f2ab586fe809
Maria Fernanda <85709697+flanconi@users.noreply.github.com>
2021-11-19 16:59:13 -0300

    nao finalizado

--- modulo02/projeto-labefy/projeto-labefy/src/App.js
+++ modulo02/projeto-labefy/projeto-labefy/src/App.js
@@ -1,12 +1,17 @@
 import react from "react";
-import axios from  "axios";
+import axios from "axios";
 
 
 export default class App extends react.Component {
 
   state = {
     playlistNameInput: '',
-    playlistList: []
+    musicNameInput: '',
+    artistNameInput: '',
+    urlMusicInput: '',
+    playlistId: '',
+    playlistList: [],
+    playlistTracksList: []
   }
 
   componentDidMount() {
@@ -14,7 +19,19 @@ export default class App extends react.Component {
   }
 
   handlePlaylistNameInput = (e) => {
-    this.setState({playlistNameInput: e.target.value})
+    this.setState({ playlistNameInput: e.target.value })
+  }
+
+  handleMusicNameInput = (e) => {
+    this.setState({ musicNameInput: e.target.value })
+  }
+
+  handleArtistNameInput = (e) => {
+    this.setState({ artistNameInput: e.target.value })
+  }
+
+  handleUrlMusicInput = (e) => {
+    this.setState({ urlMusicInput: e.target.value })
   }
 
   createPlaylist = () => {
@@ -26,14 +43,14 @@ export default class App extends react.Component {
         Authorization: 'maria-lanconi-carver'
       }
     })
-    .then(() => {
-      this.setState({playlistNameInput: ''}) 
-      alert('Playlist criada com sucesso!')
-    })
-    .catch((error) => {
-      console.log(error.response.data)
-      alert('Não foi possível criar sua playlist :( ')
-    })
+      .then(() => {
+        this.setState({ playlistNameInput: '' })
+        alert('Playlist criada com sucesso!')
+      })
+      .catch((error) => {
+        console.log(error.response.data)
+        alert('Não foi possível criar sua playlist :( ')
+      })
   }
 
   getPlaylist = () => {
@@ -42,36 +59,78 @@ export default class App extends react.Component {
         Authorization: 'maria-lanconi-carver'
       }
     })
-    .then((res) => {
-      this.setState({playlistList: res.data.result.list})
+      .then((res) => {
+        this.setState({ playlistList: res.data.result.list })
+      })
+      .catch((error) => {
+        console.log(error.response.data)
+        alert('Houve um problema, tente novamente :(')
+      })
+  }
+
+  deletePlaylist = (id) => {
+    axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}`, {
+      headers: {
+        Authorization: 'maria-lanconi-carver'
+      }
     })
-    .catch((error) => {
-      console.log(error.response.data)
-      alert('Houve um problema, tente novamente :(')
+      .then((res) => {
+        alert('Playlist deletada com sucesso!')
+        this.getPlaylist()
+      })
+      .catch((error) => {
+        alert('Não foi possível deletar sua playlist!')
+        console.log(error.response.data)
+      })
+  }
+
+  getPlaylistTracks = (id) => {
+    axios.get(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}/tracks`, {
+      headers: {
+        Authorization: 'maria-lanconi-carver'
+      }
     })
+      .then((res) => {
+        this.setState({ playlistTracksList: res.data.result.tracks, playlistCreated: id })
+      })
+      .catch((error) => {
+        console.log(error.response.data)
+      })
   }
 
   render() {
 
+    const playlistTracks = this.state.playlistTracksList.map((item) => {
+      return <div key={item.id}>
+        {item.name}
+      </div>
+    })
+
     const playlistCreated = this.state.playlistList.map((item) => {
       return <div key={item.id}>
-          {item.name}
+
+        {item.name}
+
+        <button onClick={() => this.deletePlaylist(item.id)}>Excluir</button>
+        <button onClick={() => this.getPlaylistTracks(item.id)}>+</button>
+
+        {playlistTracks}
+
       </div>
-  })
-    
+    })
+
     return (
-      <div> 
-         <p>Adicione um nome para sua playlist</p>
+      <div>
+        <p>Adicione um nome para sua playlist</p>
 
-         <input
-         placeholder = 'Nome playlist'
-         value = {this.state.playlistNameInput}
-         onChange = {this.handlePlaylistNameInput}
-         />
+        <input
+          placeholder='Nome playlist'
+          value={this.state.playlistNameInput}
+          onChange={this.handlePlaylistNameInput}
+        />
+        <button onClick={this.createPlaylist}>Criar Playlist</button>
 
-         <button onClick = {this.createPlaylist}>Criar Playlist</button>
-         
-         {playlistCreated}
+        {this.state.playlistList.length > 0 ? playlistCreated : 'Crie sua primeira playlist :D'}
       </div>
     )
   }
