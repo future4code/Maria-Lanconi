47e8c642b0746f71e56b3d292a3f12000722f960
Maria Fernanda <85709697+flanconi@users.noreply.github.com>
2022-01-20 17:25:42 -0300

    Exercicio OK

---
+++ modulo04/aprofundamento-typescript/src/exe04.ts
@@ -0,0 +1,26 @@
+type pokemon = {
+	name: string,
+  types: string,
+	healthPoints: number
+}
+
+const pokemon1: pokemon = {
+  name: "Charmander",
+  types: "Fire",
+  healthPoints: 28
+}
+
+const pokemon2: pokemon = {
+  name: "Bulbasaur",
+  types: "Grass/Poison",
+  healthPoints: 31
+}
+
+const pokemon3: pokemon = {
+  name: "Squirtle",
+  types: "Water",
+  healthPoints: 35
+}
+
+//tsc ./src/exercicio-4.ts ou configuraria isso no start e rodaria npm start
+//adicionaria a nome a pasta antes do nome do arquivo
