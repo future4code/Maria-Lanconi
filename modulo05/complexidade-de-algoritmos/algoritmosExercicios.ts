bde618ba42366a68708bdf255739d8f1ad62d430
Maria Fernanda <85709697+flanconi@users.noreply.github.com>
2022-03-29 19:01:30 -0300

    algoritmosExercicios

---
+++ modulo05/complexidade-de-algoritmos/algoritmosExercicios.ts
@@ -0,0 +1,75 @@
+//Exercicio 01
+
+const findFirstRecurringCharacter = (input) => {
+    const charHashMap = {};
+    for (const char of input) {
+      if (charHashMap[char] === true) {
+        return char;
+      }
+      charHashMap[char] = true;
+    }
+    return null;
+}; 
+
+//complexidade O(n)
+
+//Exercicio 02
+
+export const func = (
+    source: string,
+    comparison: string
+  ): boolean => {
+    if (
+      comparison.length > source.length + 1 ||
+      comparison.length < source.length - 1
+    ) {
+      return false;
+    }
+    let commonCharQuantity = 0;
+  
+    for (const char of comparison) {
+      if (source !== comparison) {
+        commonCharQuantity++;
+      }
+    }
+    return (
+      commonCharQuantity <= source.length + 1 &&
+      commonCharQuantity >= source.length - 1
+    );
+};
+
+//complexidade O(n)
+
+//Exercicio 03
+
+export const replaceMatrixValue = (
+    matrix: number[][],
+    rowIndex: number,
+    columnIndex: number,
+    value: number
+  ): void => {
+    if (
+      matrix[rowIndex] === undefined ||
+      matrix[rowIndex][columnIndex] === undefined
+    ) {
+      throw new Error("Fora do intervalo da matriz");
+    }
+  
+    matrix[rowIndex][columnIndex] = value;
+};
+
+//complexidade O(1)
+
+
+//Exercicio 04
+
+function verifyIfExistRepeatedNumbers(listOfNumbers: number[]): boolean {
+    for (let i = 0; i < listOfNumbers.length; i++) {
+      if (listOfNumbers.indexOf(listOfNumbers[i]) !== i) {
+        return true;
+      }
+    }
+    return false;
+}
+
+//complexidade O(n²)
